980ae37e660cf0178ff4a8dc33ae4f12f41ab782
Matt Ritter <matthew.d.ritter@gmail.com>
2018-12-15 00:34:57 -0600

    Simplify day 02 star 2

--- day02/star2.js
+++ day02/star2.js
@@ -3,19 +3,6 @@ const fs = require('fs');
 const rawInput = fs.readFileSync('./input.txt', 'utf8');
 const lines = rawInput.split(/\n/);
 
-function differentCharacterCount(strA, strB) {
-  const shorterStrLength = strA.length < strB.length ? strA.length : strB.length;
-
-  let diff = 0;
-  for (let i = 0; i < shorterStrLength; i++) {
-    if (strA[i] !== strB[i]) {
-      diff++;
-    }
-  }
-
-  return diff + Math.abs(strA.length - strB.length);
-}
-
 function stringIntersection(strA, strB) {
   const shorterStrLength = strA.length < strB.length ? strA.length : strB.length;
 
@@ -33,8 +20,9 @@ function stringIntersection(strA, strB) {
 // (i'm not crazy about this nested loop, but haven't thought of a better way)
 for (let i = 0; i < lines.length; i++) {
   for (let j = i + 1; j < lines.length; j++) {
-    if (differentCharacterCount(lines[i], lines[j]) === 1) {
-      console.log(stringIntersection(lines[i], lines[j]));
+    const intersection = stringIntersection(lines[i], lines[j]);
+    if (intersection.length === lines[i].length - 1 && intersection.length === lines[j].length - 1) {
+      console.log(intersection);
     }
   }
 }
